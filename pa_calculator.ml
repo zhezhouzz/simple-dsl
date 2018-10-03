8028d8e8c09a28625c3b968a51a58269fd12137f
zhouzhezz <zorichzorich@gmail.com>
2018-10-02 21:40:09 -0400

    Add simple int ast interpreter

--- pa_calculator.ml
+++ pa_calculator.ml
@@ -1,10 +1,12 @@
 open Camlp4.PreCast
+module MyUsers = Map.Make(String);;
 
-type vec =
-    | Scalar of string
-    | Vector of string list
-    | Sum of vec * vec
-    | ScalarProduct of vec * vec
+type expr =
+    | Num of int
+    | Variable of string
+    | Plus of expr * expr
+    | Multi of expr * expr
+    | Minus of expr * expr
 
 let expression = Gram.Entry.mk "expression"
 
@@ -12,57 +14,53 @@ EXTEND Gram
   GLOBAL: expression;
 
   expression:
-    [ "sum" LEFTA
-      [ x = SELF; "+"; y = SELF -> Sum (x, y) ]
-    | "scalar" LEFTA
-      [ x = SELF; "*"; y = SELF -> ScalarProduct (x, y) ]
-    | "simple" NONA
-      [ "("; e = SELF; ")" -> e
-      | s = scalar -> Scalar s
-      | v = vector -> v ] ];
-
-  scalar:
-    [ [ `INT (i, _) -> string_of_float (float i)
-    | `FLOAT (_, f) -> f ] ];
-
-  vector:
-    [ [ "["; v = LIST1 [ s = scalar -> s ] SEP ","; "]" -> Vector v ] ];
-
+    [ "Plus" LEFTA
+      [ x = SELF; "+"; y = SELF -> Plus (x, y) ]
+    | "Multi" LEFTA
+      [ x = SELF; "*"; y = SELF -> Multi (x, y) ]
+    | "Minus" LEFTA
+      [ x = SELF; "-"; y = SELF -> Minus (x, y) ]
+    | "NumInt" NONA
+      [ `INT (i, _) -> Num i]
+    | "NumString" NONA
+      [ s = LIDENT -> Variable s] ];
 END
+
+type state = int MyUsers.t
 
 let _loc = Loc.mk "<string>"
 
+exception PaserError of string
+
 let rec generate_code = function
-    | Scalar s -> <:expr< $flo:s$ >>
-    | Vector vlist -> List.fold_right
-            (fun x l -> <:expr< $flo:x$ :: $l$ >>)
-             vlist
-            <:expr< [] >>
-    | Sum (v1, v2) -> let ast_v1 = generate_code  v1
-          and ast_v2 = generate_code  v2
-          in
-            (match (v1, v2) with
-            | Scalar _, Scalar _    -> <:expr< ( +. ) $ast_v1$ $ast_v2$ >>
-            |  _, _         -> <:expr< List.map2 ( +. ) $ast_v1$ $ast_v2$ >>)
-    | ScalarProduct (v1, v2) ->let ast_v1 = generate_code  v1
-               and ast_v2 = generate_code  v2
-               in
-                (match (v1, v2) with
-                | Scalar _, Scalar _ -> <:expr< ( *. ) $ast_v1$ $ast_v2$ >>
-                | Scalar _, _        -> <:expr< List.map ( fun a -> a *. $ast_v1$ ) $ast_v2$  >>
-                | _, Scalar _        -> <:expr< List.map ( fun a -> a *. $ast_v2$ ) $ast_v1$ >>
-                | _, _           -> <:expr< List.fold_right
-                                                               ( +. )
-                                                               (List.map2 ( *. ) $ast_v1$ $ast_v2$ )
-                                                                0. >>)
+    | Num i -> (Some i, let i_str = string_of_int i in <:expr< $int:i_str$ >>)
+    | Variable s -> (* let i = 1
+                    let i = (try (MyUsers.find s st) with Not_found -> raise (PaserError "PaserError")) in *)
+                    (Some 1, let i_str = string_of_int 1 in <:expr< $int:i_str$ >>)
+    | Plus (v1, v2) -> let ast_v1 = generate_code  v1
+                       and ast_v2 = generate_code  v2
+                       in (match (ast_v1, ast_v2) with
+                       | ((Some i), _), ((Some j), _) -> let _str = string_of_int (i+j) in ((Some (i+j)), <:expr< $int:_str$ >>)
+                       | (_, code1), (_, code2)       -> raise (PaserError "PaserError"))
+    | Multi (v1, v2) -> let ast_v1 = generate_code  v1
+                       and ast_v2 = generate_code  v2
+                       in (match (ast_v1, ast_v2) with
+                       | ((Some i), _), ((Some j), _) -> let _str = string_of_int (i*j) in ((Some (i*j)), <:expr< $int:_str$ >>)
+                       | (_, code1), (_, code2)       -> raise (PaserError "PaserError"))
+    | Minus (v1, v2) -> let ast_v1 = generate_code  v1
+                       and ast_v2 = generate_code  v2
+                       in (match (ast_v1, ast_v2) with
+                       | ((Some i), _), ((Some j), _) -> let _str = string_of_int (i-j) in ((Some (i-j)), <:expr< $int:_str$ >>)
+                       | (_, code1), (_, code2)       -> raise (PaserError "PaserError"))
 
 let parse_and_generate_code str =
   let e = Gram.parse_string expression _loc str in
-   generate_code e
+   match (try (generate_code e) with (PaserError err_code) -> (None, <:expr< $str:err_code$ >>)) with
+   | _, code -> code
 
 let _ =
     print_string "# ";
     let str = read_line () in
     let e = parse_and_generate_code str in
         let ast_e = <:expr< $e$ >> in
-        Camlp4.PreCast.Printers.OCaml.print_implem <:str_item< let _ = let res = $ast_e$ in print_float res >>
+        Camlp4.PreCast.Printers.OCaml.print_implem <:str_item< let _ = let res = $ast_e$ in print_int res >>
